3b1bbd9c90394b1216c6b88e262a52a0b65fdcd7
cuzit <mtsilvey@gmail.com>
2012-11-03 19:41:32 -0400

    Added some zips you'll need to unzip and add the jar inside to your classpath.

--- Development/Credentials.java
+++ Development/Credentials.java
@@ -7,18 +7,27 @@ public class Credentials extends JFrame implements ActionListener {
   /**********
   *Variables*
   ***********/
+  //JButtons
   private JButton ok;
   private JButton cancel;
+  
+  //JLabels
   private JLabel user;
   private JLabel pass;
   private JLabel instructions;
+  
+  //JTextFields
   private JTextField userBox;
   private JTextField passBox;
+  
+  //Strings
   private String username;
   private String password;
-  private boolean complete;
-  private OnContactListener listener;
   private String creds;
+  
+  //OnContactListener
+  private OnContactListener listener;
+  
   
   /************
   *Constructor*
@@ -26,35 +35,43 @@ public class Credentials extends JFrame implements ActionListener {
   public Credentials(OnContactListener l) {
     //Initialize components
     listener = l;
+    
     username = ""; password = "";
+    
     ok = new JButton("OK");
     ok.addActionListener(this);
     cancel = new JButton("Cancel");
     cancel.addActionListener(this);
+    
     user = new JLabel("Username:");
     pass = new JLabel("Password:");
     instructions = new JLabel("Please enter your GMail credentials:");
+    
     userBox = new JTextField(20);
     passBox = new JTextField(20);
-    complete = false;
+    
     
     //Set up username area
     JPanel userArea = new JPanel(new GridLayout(2, 1));
     userArea.add(user);
     userArea.add(userBox);
     
+    
     //Set up password area
     JPanel passArea = new JPanel(new GridLayout(2, 1));
     passArea.add(pass);
     passArea.add(passBox);
+    
     
     //Set up button area
     JPanel buttonArea = new JPanel();
     buttonArea.add(ok);
     buttonArea.add(cancel);
     
+    
     //Set up window
     setLayout(new GridLayout(4, 1));
+    
     
     //Add panels to main window
     add(instructions);
@@ -63,6 +80,7 @@ public class Credentials extends JFrame implements ActionListener {
     add(buttonArea);
   }
   
+  
   /******************
   *OnContactListener*
   *******************/
@@ -70,23 +88,29 @@ public class Credentials extends JFrame implements ActionListener {
     public void onData(String s);
   }
   
+  
   /***************
   *ActionListener*
   ****************/
   public void actionPerformed(ActionEvent event) {
+    //If user presses OK button
     if (event.getSource() == ok) {
+      //If both username and password fields are empty
       if(userBox.getText() == "" && passBox.getText() == "") {
 	JOptionPane.showMessageDialog(this, "Username and password cannot be blank!");
       }
       
+      //If username field is empty
       else if(userBox.getText() == "") {
 	JOptionPane.showMessageDialog(this, "Username cannot be blank!");
       }
       
+      //If password field is empty
       else if(passBox.getText() == "") {
 	JOptionPane.showMessageDialog(this, "Password cannot be blank!");
       }
       
+      //If there exists information for both the username and password fields
       else {
 	username = userBox.getText();
 	password = passBox.getText();
@@ -97,27 +121,14 @@ public class Credentials extends JFrame implements ActionListener {
       }
     }
     
+    //If the user presses the cancel button
     else if (event.getSource() == cancel) {
       dispose();
     }
     
+    //Fail-safe
     else {
       JOptionPane.showMessageDialog(this, "An unknown error has occurred.");
     }
-  }
-  
-  /********************
-  *Getters and Setters*
-  *********************/
-  public String getUsername() {
-    return username;
-  }
-  
-  public String getPassword() {
-    return password;
-  }
-  
-  public boolean getComplete() {
-    return complete;
   }
 }
--- Development/EmailSender.java
+++ Development/EmailSender.java
@@ -3,22 +3,37 @@ import java.awt.*;
 import javax.swing.*;
 import java.awt.event.*;
 
+import javax.mail.*;
+import javax.mail.internet.*;
+import java.util.Properties;
+
 public class EmailSender extends JFrame implements ActionListener {
   /**********
   *Variables*
   ***********/
+  //Buttons
   private JButton to;
-  private JLabel from;
-  private JTextArea messageBox;
-  private JTextField subjectBox;
+  private JButton send;
   private JButton clear;
-  private String username;
-  private String password;
-  private JTextField fromBox;
-  private JTextField toBox;
+  
+  //Labels
+  private JLabel from;
   private JLabel subject;
   private JLabel message;
-  private JButton send;
+  
+  //JTextFields
+  private JTextField subjectBox;
+  private JTextField fromBox;
+  private JTextField toBox;
+  
+  //JTextAreas
+  private JTextArea messageBox;
+  
+  //Strings
+  private String username;
+  private String password;
+  private String[] creds;
+  
   
   /************
   *Constructor*
@@ -29,31 +44,39 @@ public class EmailSender extends JFrame implements ActionListener {
     to.addActionListener(this);
     clear = new JButton("Clear");
     clear.addActionListener(this);
-    from = new JLabel("From:");
-    subjectBox = new JTextField(300);
-    messageBox = new JTextArea(300, 200);
-    username = ""; password = "";
-    fromBox = new JTextField(300);
-    toBox = new JTextField(300);
-    subject = new JLabel("Subject:");
-    message = new JLabel("Message:");
     send = new JButton("Send");
     send.addActionListener(this);
+    
+    from = new JLabel("From:");
+    subject = new JLabel("Subject:");
+    message = new JLabel("Message:");
+    
+    fromBox = new JTextField(300);
+    toBox = new JTextField(300);
+    subjectBox = new JTextField(300);
+    
+    messageBox = new JTextArea(300, 200);
+
+    username = ""; password = "";
+    
     
     //Create panel containing to area
     JPanel toArea = new JPanel(new GridLayout(2,1));
     toArea.add(to);
     toArea.add(toBox);
     
+    
     //Create panel containing from area
     JPanel fromArea = new JPanel(new GridLayout(2, 1));
     fromArea.add(from);
     fromArea.add(fromBox);
     
+    
     //Create panel containing subject area
     JPanel subjectArea = new JPanel(new GridLayout(2, 1));
     subjectArea.add(subject);
     subjectArea.add(subjectBox);
+    
     
     //Add the toArea,fromArea, and subjectArea panels into one panel
     JPanel inputs = new JPanel(new GridLayout(3, 1));
@@ -61,10 +84,12 @@ public class EmailSender extends JFrame implements ActionListener {
     inputs.add(fromArea);
     inputs.add(subjectArea);
     
+    
     //Make the Message area
     JPanel messageArea = new JPanel(new GridLayout(2, 1));
     messageArea.add(message);
     messageArea.add(messageBox);
+    
     
     //Set up other buttons
     JPanel buttonArea = new JPanel();
@@ -74,6 +99,7 @@ public class EmailSender extends JFrame implements ActionListener {
     
     //Set main panel properties
     setLayout(new GridLayout(3, 1));
+    
     
     //Add everything
     add(inputs);
@@ -85,22 +111,27 @@ public class EmailSender extends JFrame implements ActionListener {
   *ActionListener*
   ****************/
   public void actionPerformed(ActionEvent event) {
+    //If send is pressed
     if (event.getSource() == send) {
-      JFrame credWindow = new JFrame();
-      Credentials credPanel = new Credentials(new Credentials.OnContactListener() {
+      Credentials credWindow = new Credentials(new Credentials.OnContactListener() {
+	//This code is executed when the frame is closed and a value for s is
+	//returned.
 	public void onData(String s) {
+	  System.out.println("s: " + s);
+	  String[] creds = s.split(" ");
+	  System.out.println("Username: " + creds[0]);
+	  System.out.println("Password: " + creds[1]);
+	  
 	}
-	
       });
-      credWindow.add(credPanel);
       credWindow.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
       credWindow.setTitle("Enter Credentials");
       credWindow.setPreferredSize(new Dimension(300, 200));
       credWindow.pack();
       credWindow.setVisible(true);
-      
     }
     
+    //If clear is pressed
     else if (event.getSource() == clear) {
       toBox.setText("");
       fromBox.setText("");
@@ -109,11 +140,15 @@ public class EmailSender extends JFrame implements ActionListener {
       JOptionPane.showMessageDialog(this, "Fields have been reset.");
     }
     
+    //If to is pressed
     else if (event.getSource() == to) {
       JFrame contactWindow = new JFrame();
       ContactUI contactPanel = new ContactUI(new ContactUI.OnContactListener()
       {
+	//This code is executed when the frame is closed and a value for s is
+	//returned.
 	public void onData(String s){
+	  //blah
 	}
       });
       contactWindow.add(contactPanel);
@@ -123,10 +158,12 @@ public class EmailSender extends JFrame implements ActionListener {
       contactWindow.setVisible(true);
     }
     
+    //Fail-safe
     else {
       JOptionPane.showMessageDialog(this, "An unknown error has occurred.");
     }
   }
+  
   
   /*****
   *Main*
